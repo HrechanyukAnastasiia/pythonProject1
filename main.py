7721074fa41682a738efe6a306b0e164cfe3d09c
student <student@PC-H-04>
2023-05-18 20:30:29 +0300

    друге завдання

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#2
+my_list = [1,2,3,4,5,6,7,8,9,10]
+class SquareGenerator:
+    def __init__(self):
+        if isinstance(my_list, list) is True:
+            print(my_list)
+        elif isinstance(my_list, list) is not True:
+            TypeError("Помилка!")
+    def my_generator(self, name):
+        for num in my_list:
+            num = num ** 2
+            yield num
+    for num in my_generator("", my_list):
+        print(num)
