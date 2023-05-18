16e849847ee18c0da30fe08bbdcfc6944b894973
student <student@PC-H-04>
2023-05-18 20:37:08 +0300

    третє завдання

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#3
+class ListIterator:
+    def __init__(self, lst):
+        self.data = lst
+        self.index = 0
+    def __iter__(self):
+        return self
+    def __next__(self):
+        if self.index >= len(self.data):
+            raise StopIteration
+        value = self.data[self.index]
+        self.index += 1
+        return value
+my_list = [1,2,3,4,5,6,7,8,9,10]
+my_iter = ListIterator(my_list)
+for num in my_list:
+    print(num)
